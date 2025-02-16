b6c7424c8e15cb389cb609c12511652ba7a26769
Yeongho Kim <yeongho52@naver.com>
2025-02-17 02:55:34 +0900

    [fix] PostSignupReqDto.java 수정

--- src/main/java/com/baro/baro_assignment/auth/model/dto/PostSignupReqDto.java
+++ src/main/java/com/baro/baro_assignment/auth/model/dto/PostSignupReqDto.java
@@ -1,5 +1,10 @@
 package com.baro.baro_assignment.auth.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class PostSignupReqDto {
 
     private String username;
